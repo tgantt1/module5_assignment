54a2465f137a12e3d397db41f4fbac1338bb2767
tgantt1 <trey.gantt@gmail.com>
2026-06-23 06:44:22 -0400

    memento, exception, hisory files added

---
+++ app/exceptions.py
@@ -0,0 +1,14 @@
+class CalculatorError(Exception):
+    pass
+
+
+class ValidationError(CalculatorError):
+    pass
+
+
+class OperationError(CalculatorError):
+    pass
+
+
+class ConfigurationError(CalculatorError):
+    pass
